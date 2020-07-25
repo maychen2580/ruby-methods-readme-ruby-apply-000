7f702cb929aa17507509b4d9de2648a009b7c61a
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-07-25 18:13:46 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,6 @@
 # Use this space to code along with the readme
-
-phrase = "Hello World!"
+def say_hello_ten_times
+  phrase = "Hello World!"
 puts phrase
 puts phrase
 puts phrase
@@ -11,3 +11,7 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+end
+say_hello_ten_times
+say_hello_ten_times
+puts "Hello World!"
